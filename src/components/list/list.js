80e9e644a461ec94f62ce45aee3458839ce4970e
Jesús Bossa <me@jesusbossa.com>
2019-04-07 15:36:17 -0500

    [IMPROVEMENT] List layout

--- src/components/list/list.js
+++ src/components/list/list.js
@@ -1,26 +1,34 @@
 import React from 'react'
 import './list.css'
 
+const ListCard = () => (
+  <div className="list-card">
+    <div className="list-card-media">
+      <img src="https://picsum.photos/250/150" alt="" />
+    </div>
+    <div className="list-card-description">
+      <h3>Sándwich pequeño</h3>
+      <span>16k</span>
+    </div>
+    <div className="list-card-action">
+      <button>+</button>
+    </div>
+  </div>
+)
+
 export default class extends React.Component {
   render() {
     return (
-      <div className="list">
-      
+      <div className="list-section">
+
         <div className="list-header">
           <h2>Sándwich</h2>
         </div>
-
-        <div className="list-card">
-          <div className="list-card-media">
-            <img src="https://picsum.photos/250/150" alt="" />
-          </div>
-          <div className="list-card-description">
-            <h3>Sándwich pequeño</h3>
-            <span>16k</span>
-          </div>
-          <div className="list-card-action">
-            <button>+</button>
-          </div>
+        
+        <div className="list-container">
+          <ListCard />
+          <ListCard />
+          <ListCard />
         </div>
 
       </div>
