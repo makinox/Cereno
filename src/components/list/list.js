cbf0b49e7195b911d8baa4e1911d7335134c312d
Jesús Bossa <me@jesusbossa.com>
2019-04-08 18:57:34 -0500

    [ADD] List improved

--- src/components/list/list.js
+++ src/components/list/list.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import './list.css'
 
-const ListCard = () => (
+const ListCard = (props) => (
   <div className="list-card">
     <div className="list-card-media">
       <img src="https://picsum.photos/250/150" alt="" />
     </div>
     <div className="list-card-description">
-      <h3>Sándwich pequeño</h3>
-      <span>16k</span>
+      <h3>{props.title || 'Sándwich pequeño'}</h3>
+      <span>{props.money || '16k'}</span>
     </div>
     <div className="list-card-action">
       <button>+</button>
@@ -26,9 +26,41 @@ export default class extends React.Component {
         </div>
         
         <div className="list-container">
-          <ListCard />
-          <ListCard />
-          <ListCard />
+          <ListCard title="Sándwich grande" money="16k" />
+          <ListCard title="Sándwich mixto" money="10k" />
+          <ListCard title="Sándwich pequeño" money="8k" />
+        </div>
+
+        <div className="list-mobile">
+          <ListCard title="Sándwich grande" money="16k" />
+        </div>
+
+        <div className="list-header">
+          <h2>Perro</h2>
+        </div>
+        
+        <div className="list-container">
+          <ListCard title="Perro grande" money="16k" />
+          <ListCard title="Perro mixto" money="10k" />
+          <ListCard title="Perro pequeño" money="8k" />
+        </div>
+
+        <div className="list-mobile">
+          <ListCard title="Perro grande" money="16k" />
+        </div>
+
+        <div className="list-header">
+          <h2>Salchipapa</h2>
+        </div>
+        
+        <div className="list-container">
+          <ListCard title="Salchipapa grande" money="16k" />
+          <ListCard title="Salchipapa mixto" money="10k" />
+          <ListCard title="Salchipapa pequeño" money="8k" />
+        </div>
+
+        <div className="list-mobile">
+          <ListCard title="Salchipapa grande" money="16k" />
         </div>
 
       </div>
