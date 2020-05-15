dda3a14d6429580f06d7048f0152a53541cfa5bb
Jesus bossa <x@jesusbossa.dev>
2020-05-15 10:58:04 -0500

    [ADD] RE: Start

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+import { Link } from "gatsby"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <>
+    <SEO title="Home" />
+    <h1>Hi people</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </>
+)
+
+export default IndexPage
